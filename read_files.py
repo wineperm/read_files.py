826614d0eb54d1a62992b938e581965b5c443d4e
wineperm <leo_kraska@mail.ru>
2024-10-23 13:33:41 +0500

    Update read_files.py

--- read_files.py
+++ read_files.py
@@ -8,16 +8,29 @@ def read_files_recursively(root_dir, output_file, exclude_files, exclude_dirs):
             for filename in filenames:
                 file_path = os.path.join(dirpath, filename)
                 if filename not in exclude_files:
+                    # Относительный путь к файлу
+                    relative_path = os.path.relpath(file_path, root_dir)
                     with open(file_path, 'r', encoding='utf-8', errors='ignore') as in_file:
                         content = in_file.read()
-                        out_file.write(f"{file_path}\n")
+                        out_file.write(f"{relative_path}\n")
                         out_file.write(content)
                         out_file.write("\n\n")
 
 if __name__ == "__main__":
+    # Текущая директория, в которой находится скрипт
     current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Путь к выходному файлу, в который будет записано содержимое всех файлов
     output_file = os.path.join(current_dir, 'output.txt')
+
+    # Множество имен файлов, которые нужно исключить из обхода
     exclude_files = {'output.txt', 'read_files.py', 'generate_project_structure.py'}
+
+    # Множество имен директорий, которые нужно исключить из обхода
     exclude_dirs = {'.git'}
+
+    # Вызов функции для рекурсивного чтения файлов
     read_files_recursively(current_dir, output_file, exclude_files, exclude_dirs)
+
+    # Сообщение о завершении работы скрипта
     print(f"Содержимое всех файлов записано в {output_file}")
